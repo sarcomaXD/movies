11945f339021e24ad528cff493024432495c0af9
sarcomaXD <116233913+sarcomaXD@users.noreply.github.com>
2023-07-19 20:13:51 +0900

    PUBLIC_URL

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function Movie({ id, title, poster_path }) {
   return (
     <div className="windows">
-      <Link to={`/movie/${id}`}>
+      <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>
         <img
           src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`}
           alt={title}
