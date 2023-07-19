8836dbe9b6432dee028c2a5450787aa253e25000
sarcomaXD <116233913+sarcomaXD@users.noreply.github.com>
2023-07-19 20:10:31 +0900

    router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Home from "./views/home";
 import Detail from "./views/Detail";
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        ></Route>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
